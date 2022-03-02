41c09275d8c2c6cd63eebe8b8c0123c06c336218
Alexandre De Pablo <alexandre.depablo@gmail.com>
2022-03-02 12:19:13 +0100

    Introduce root page
    Add "hello world" page for heroku testing.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   mount Slack::API => '/'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
 end
