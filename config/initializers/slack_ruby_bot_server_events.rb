88e46ba2b56e1862c8a5b3968e2758bddf6f7f4b
Alexandre De Pablo <alexandre.depablo@gmail.com>
2022-03-07 13:22:06 +0100

    Scumbag me
    Add handler for incoming Slack events

--- config/initializers/slack_ruby_bot_server_events.rb
+++ config/initializers/slack_ruby_bot_server_events.rb
@@ -1,3 +1,8 @@
 SlackRubyBotServer::Events.configure do |config|
   config.signing_secret ||= ENV.fetch('SLACK_SIGNING_SECRET')
+
+  config.on :event, 'event_callback' do |event|
+    result = SlackEvent::CreateNewPost.call(event: event)
+    result.success?
+  end
 end
